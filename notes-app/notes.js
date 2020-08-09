e55b8488025b8a7c220b6cb5f1276545268e6282
Donald Fox <fox_donald@yahoo.com>
2020-08-09 02:16:08 -0400

    Implement read note capabilities

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,16 +1,12 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-    return 'Your notes...'
-}
-
 const addNote = (title, body) => {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -25,6 +21,7 @@ const addNote = (title, body) => {
 
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
+
     fs.writeFileSync('notes.json', dataJSON)
 }
 
@@ -53,15 +50,29 @@ const removeNote = (title) => {
 
 const listNotes = () => {
     const notes = loadNotes()
+
     console.log(chalk.green.inverse('Your notes'))
     notes.forEach((note) => {
         console.log(note.title)
     })
 }
 
+const readNote = (title) => {
+    const notes = loadNotes()
+
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found'))
+    }
+}
+
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
