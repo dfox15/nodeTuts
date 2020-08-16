842120f7329d9f6607cd5adeef5d498a3173d426
Donald Fox <fox_donald@yahoo.com>
2020-08-16 14:22:21 -0400

    Abstract geocoding request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const dotenv = require('dotenv')
+const geocode = require('./utils/geocode')
 const API_KEY = process.env.API_KEY
 
 // const url =
@@ -52,28 +53,7 @@ const API_KEY = process.env.API_KEY
 //     }
 // )
 
-const geocode = (address, callback) => {
-    const url =
-        'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-        encodeURIComponent(address) +
-        '.json?access_token=pk.eyJ1IjoiZGZveDE1IiwiYSI6ImNrZG91N3J3ajF2dG0yeGxqeW9rd2RsMGYifQ.6iKGnN1sYyrtOOUE9cF2jQ&limit=1'
-
-    request({ url: url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-}
-
-geocode('Philadelphia New York', (error, data) => {
+geocode('Boston', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
