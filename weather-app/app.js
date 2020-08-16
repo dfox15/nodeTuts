7f6a7220cf83fa809a66c34630551b26f45addda
Donald Fox <fox_donald@yahoo.com>
2020-08-16 14:17:46 -0400

    Implemented geocoding callback with dynamic location

--- weather-app/app.js
+++ weather-app/app.js
@@ -30,24 +30,50 @@ const API_KEY = process.env.API_KEY
 //     }
 // )
 
-const geocodeURL =
-    'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGZveDE1IiwiYSI6ImNrZG91N3J3ajF2dG0yeGxqeW9rd2RsMGYifQ.6iKGnN1sYyrtOOUE9cF2jQ&limit=1'
+// const geocodeURL =
+//     'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGZveDE1IiwiYSI6ImNrZG91N3J3ajF2dG0yeGxqeW9rd2RsMGYifQ.6iKGnN1sYyrtOOUE9cF2jQ&limit=1'
 
-request(
-    {
-        url: geocodeURL,
-        json: true
-    },
+// request(
+//     {
+//         url: geocodeURL,
+//         json: true
+//     },
 
-    (error, response) => {
+//     (error, response) => {
+//         if (error) {
+//             console.log('Unable to connect to location services')
+//         } else if (response.body.features.length === 0) {
+//             console.log('Unable to find location. Try another search!')
+//         } else {
+//             var latitude = response.body.features[0].center[1]
+//             var longitude = response.body.features[0].center[0]
+//             console.log('Latitude: ' + latitude + '\n' + 'Longitude: ' + longitude)
+//         }
+//     }
+// )
+
+const geocode = (address, callback) => {
+    const url =
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+        encodeURIComponent(address) +
+        '.json?access_token=pk.eyJ1IjoiZGZveDE1IiwiYSI6ImNrZG91N3J3ajF2dG0yeGxqeW9rd2RsMGYifQ.6iKGnN1sYyrtOOUE9cF2jQ&limit=1'
+
+    request({ url: url, json: true }, (error, response) => {
         if (error) {
-            console.log('Unable to connect to location services')
+            callback('Unable to connect to location services!', undefined)
         } else if (response.body.features.length === 0) {
-            console.log('Unable to find location. Try another search!')
+            callback('Unable to find location. Try another search.', undefined)
         } else {
-            var latitude = response.body.features[0].center[1]
-            var longitude = response.body.features[0].center[0]
-            console.log('Latitude: ' + latitude + '\n' + 'Longitude: ' + longitude)
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
         }
-    }
-)
+    })
+}
+
+geocode('Philadelphia New York', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
