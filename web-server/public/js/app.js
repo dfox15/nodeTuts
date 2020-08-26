44223b2fd599ebebfc59e94f7a336ba227cbf677
Donald Fox <fox_donald@yahoo.com>
2020-08-26 00:31:02 -0400

    Wire up the UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,21 +4,25 @@ const url = 'http://localhost:3000/weather?address='
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
-    console.log(location)
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
 
     fetch(url + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
