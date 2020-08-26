be300c7ead663601f43460611c9e236d11fe715f
Donald Fox <fox_donald@yahoo.com>
2020-08-25 21:10:07 -0400

    Implement client-side rendering

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,21 @@
 console.log('Client side javascript file is loaded')
+
+const url = 'http://localhost:3000/weather?address=Boston'
+
+fetch(url).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+const weatherForm = document.querySelector('form')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('testing!')
+})
