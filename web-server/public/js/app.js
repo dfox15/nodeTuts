9f25612bbeb0f0025c624736944c553cb278813c
Donald Fox <fox_donald@yahoo.com>
2020-08-27 22:28:59 -0400

    Setup app for Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,6 @@
 console.log('Client side javascript file is loaded')
 
-const url = 'http://localhost:3000/weather?address='
+const url = '/weather?address='
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
