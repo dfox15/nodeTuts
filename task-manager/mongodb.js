85a5cd2d3beac3bdd5ff6becd565a1830361f18c
Donald Fox <fox_donald@yahoo.com>
2020-08-31 22:10:30 -0400

    Destructure Mongo attributes

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,6 @@
 // CRUD  create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -13,8 +12,52 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Donald',
-        age: 27
-    })
+    // db.collection('users').insertOne(
+    //     {
+    //         name: 'Donald',
+    //         age: 38
+    //     },
+    //     (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert user')
+    //         }
+
+    //         console.log(result.ops)
+    //     }
+    // )
+
+    // db.collection('users').insertMany(
+    //     [
+    //         {
+    //             name: 'Jen',
+    //             age: 28
+    //         },
+    //         {
+    //             name: 'Gunther',
+    //             age: 27
+    //         }
+    //     ],
+    //     (error, result) => {
+    //         if (error) {
+    //             console.log('Unable to insert documents!')
+    //         }
+
+    //         console.log(result.ops)
+    //     }
+    // )
+
+    // db.collection('tasks').insertMany(
+    //     [
+    //         { description: 'Wash dishes', completed: false },
+    //         { description: 'Exercise', completed: true },
+    //         { description: 'Pack baby lunch', completed: true }
+    //     ],
+    //     (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert document!')
+    //         }
+
+    //         console.log(result.ops)
+    //     }
+    // )
 })
