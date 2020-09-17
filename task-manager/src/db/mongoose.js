4643db746dab8c88bb9aaabf442abb6d272358c2
Donald Fox <fox_donald@yahoo.com>
2020-09-16 21:55:58 -0400

    Refactor model, add libraries, and add routes

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,64 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-const User = mongoose.model('users', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain string "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '          Harry          ',
-//     email: 'harry@yahoo.io',
-//     password: 'Phoned123           '
-// })
-
-// me.save()
-//     .then((me) => {
-//         console.log(me)
-//     })
-//     .catch((error) => {
-//         console.log('Error!', error)
-//     })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -72,15 +18,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const task = new Task({
-    description: 'Cook soup     '
-})
-
-task.save()
-    .then((task) => {
-        console.log(task)
-    })
-    .catch((error) => {
-        console.log('Error!', error)
-    })
