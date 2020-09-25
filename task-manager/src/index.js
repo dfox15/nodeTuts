e3dbfdfdc942f37d93c3189da5686122469f20ac
Donald Fox <fox_donald@yahoo.com>
2020-09-25 00:14:56 -0400

    Add task update endpoint

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { update } = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -96,6 +97,28 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
