db5487d75f52baaa3af61333920489c5ccffe379
Donald Fox <fox_donald@yahoo.com>
2020-09-25 14:22:06 -0400

    Implement deletion of task and user by ID

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -66,6 +66,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -119,6 +133,19 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(400).send()
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
