555569e15a3cf12cf60dc1ac9efc944c63ea2069
Donald Fox <fox_donald@yahoo.com>
2020-10-03 13:56:09 -0400

    Genrate user token and modify response with user and token

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -6,8 +6,9 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
+        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send(user)
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
