64b47111523101b57227d3f1a190d060df26f726
Donald Fox <fox_donald@yahoo.com>
2020-10-11 14:06:16 -0400

    Add authentication to Task routes and lockdown task manipulation to logged in user

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose')
 const Task = mongoose.model('Task', {
     description: {
         type: String,
-        trim: true,
-        required: true
+        required: true,
+        trim: true
     },
     completed: {
         type: Boolean,
-        required: false,
         default: false
     },
     owner: {
