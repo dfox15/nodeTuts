c981ca638ef76b8200c722456169fec569ea54a1
Donald Fox <fox_donald@yahoo.com>
2020-10-10 21:55:21 -0400

    Associate user and tasks

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const { update } = require('../models/task')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
-        await task.save()
+        //await task.save()
         res.status(201).send(task)
     } catch (e) {
         res.status(400).send(e)
