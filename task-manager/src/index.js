b63c7b0699063cc8ff2594b38d1dcb857889cb97
Donald Fox <fox_donald@yahoo.com>
2020-10-14 21:46:10 -0400

    Implement file upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,9 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     res.status(503).sendStatus('Site is currently down. Check back soon')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
