bb176971be11b9fbeb4f2e2002d68a84a48654af
Donald Fox <fox_donald@yahoo.com>
2020-10-24 12:49:51 -0400

    Sending welcome and cancellation email

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = process.env.SENDGRID_API_KEY
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: process.env.TO_EMAIL,
-    from: process.env.FROM_EMAIL,
-    subject: 'This is my first creation',
-    text: 'I hope this one actually gets to you'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: process.env.FROM_EMAIL,
+        subject: 'Thanks for joining!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: process.env.FROM_EMAIL,
+        subject: 'Regret seeing you leave!',
+        text: `${name}, we're sorry you decided to leave. Hope you reconsider.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
