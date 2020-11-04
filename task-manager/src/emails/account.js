8732ed88ed65423448f1131a1d32fb3d2554271c
Donald Fox <fox_donald@yahoo.com>
2020-11-03 23:57:25 -0500

    Implement testing and refactored files

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: process.env.FROM_EMAIL,
+        from: process.env.YAHOO_EMAIL,
         subject: 'Thanks for joining!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -15,7 +15,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: process.env.FROM_EMAIL,
+        from: process.env.YAHOO_EMAIL,
         subject: 'Regret seeing you leave!',
         text: `${name}, we're sorry you decided to leave. Hope you reconsider.`
     })
